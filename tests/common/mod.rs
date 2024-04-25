8e0f64e6f6e714351d3fab23fdfc83fa0ba93d24
Ryan <ryanford@poluglottos.com>
2024-04-25 13:48:39 +0200

    removing dead code

--- tests/common/mod.rs
+++
@@ -1,9 +0,0 @@
-// fn setup_redis_connections() -> RedisConnections {
-//     let redis_connections = RedisConnections::new().unwrap();
-//     flush(redis_connections)
-// }
-
-// fn flush(redis_connections: &RedisConnections) -> RedisConnections {
-//     redis_connections.flush_database().unwrap();
-//     redis_connections
-// }
