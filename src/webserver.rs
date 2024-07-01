858b488aedb51fff22b0fdeab9c30cec0f0e3b4e
Ryan <ryan@deltadevs.xyz>
2024-07-01 20:42:13 +0200

    fix: remove legacy CORS exceptions

--- src/webserver.rs
+++ src/webserver.rs
@@ -43,9 +43,6 @@ impl WebServer {
 
         HttpServer::new(move || {
             let cors = Cors::default()
-                .allowed_origin("http://localhost:3000")
-                .allowed_origin("http://localhost:3001")
-                .allowed_origin("https://visualizer.sebastianpusch.de")
                 .allow_any_origin()
                 .allow_any_method()
                 .allow_any_header();
