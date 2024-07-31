aa21adb9ada3d6828cd32b5b4c8f2115c94db135
Ryan <ryan@deltadevs.xyz>
2024-07-31 22:40:50 +0200

    refactor(circuits): bump IMT for Into<Scalar>, renamings, docs

--- src/circuits/less_than.rs
+++ src/circuits/less_than.rs
@@ -22,12 +22,14 @@ impl Circuit<Scalar> for LessThanCircuit {
 
         let mut result = Boolean::constant(false);
 
+        // Iterate over the bits from most significant to least significant
         for i in (0..a_bits.len()).rev() {
             let a_val = Boolean::constant(a_bits[i]);
             let b_val = Boolean::constant(b_bits[i]);
             let not_a = a_val.not();
             let not_b = b_val.not();
 
+            // Check if bits are equal (both 1 or both 0)
             let a_and_b = Boolean::and(cs.namespace(|| format!("a_and_b_{}", i)), &a_val, &b_val)?;
             let not_a_and_not_b = Boolean::and(
                 cs.namespace(|| format!("not_a_and_not_b_{}", i)),
@@ -35,9 +37,11 @@ impl Circuit<Scalar> for LessThanCircuit {
                 &not_b,
             )?;
 
+            // If the bits are equal, continue to the next bit
             if not_a_and_not_b.get_value().unwrap() || a_and_b.get_value().unwrap() {
                 continue;
             } else {
+                // If bits differ: b > a if b_bit = 1 && a_bit = 0
                 result = Boolean::and(
                     cs.namespace(|| format!("b_and_not_a_{}", i)),
                     &b_val,
@@ -47,6 +51,9 @@ impl Circuit<Scalar> for LessThanCircuit {
             }
         }
 
+        // Enforce the constraint that the result is correct
+        // If result is true, then a < b, otherwise a >= b
+        // result * (1) = 1
         cs.enforce(
             || "a < b",
             |_| result.lc(CS::one(), Scalar::one()),
