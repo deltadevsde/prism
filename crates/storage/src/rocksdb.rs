8ba536d75372e7aa2ed92c18ef0ea54cd5e6c440
Hamza Hamud <53880692+hhamud@users.noreply.github.com>
2024-09-22 18:31:57 +0100

    feat: replace todos with stuff in database

--- crates/storage/src/rocksdb.rs
+++ crates/storage/src/rocksdb.rs
@@ -1,9 +1,10 @@
 use crate::Database;
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use jmt::{
     storage::{LeafNode, Node, NodeBatch, NodeKey, TreeReader, TreeWriter},
     KeyHash, OwnedValue, Version,
 };
+use prism_errors::DatabaseError;
 use rocksdb::{DBWithThreadMode, Error, MultiThreaded, DB};
 
 type RocksDB = DBWithThreadMode<MultiThreaded>;
@@ -13,8 +14,7 @@ pub struct RocksDBConnection {
 }
 
 impl RocksDBConnection {
-    pub fn new() -> Result<RocksDBConnection, Error> {
-        let path = "";
+    pub fn new(path: &str) -> Result<RocksDBConnection, Error> {
         let db = DB::open_default(path)?;
 
         Ok(Self { connection: db })
@@ -23,7 +23,12 @@ impl RocksDBConnection {
 
 impl Database for RocksDBConnection {
     fn get_commitment(&self, epoch: &u64) -> anyhow::Result<String> {
-        todo!()
+        let key = format!("commitments:epoch_{}", epoch);
+        let value = self.connection.get(key.as_bytes())?.ok_or_else(|| {
+            DatabaseError::NotFoundError(format!("commitment from epoch_{}", epoch))
+        })?;
+
+        Ok(String::from_utf8(value)?)
     }
 
     fn set_commitment(
@@ -31,15 +36,28 @@ impl Database for RocksDBConnection {
         epoch: &u64,
         commitment: &prism_common::tree::Digest,
     ) -> anyhow::Result<()> {
-        todo!()
+        Ok(self.connection.put(
+            format!("commitments:epoch_{}", epoch).as_bytes(),
+            commitment.to_bytes(),
+        )?)
     }
 
     fn get_epoch(&self) -> anyhow::Result<u64> {
-        todo!()
+        let res = self
+            .connection
+            .get(b"app_state:epoch")?
+            .ok_or_else(|| DatabaseError::NotFoundError("current epoch".to_string()))?;
+
+        Ok(u64::from_be_bytes(
+            res.try_into()
+                .map_err(|e| DatabaseError::ReadError(e.to_string()))?,
+        ))
     }
 
     fn set_epoch(&self, epoch: &u64) -> anyhow::Result<()> {
-        todo!()
+        Ok(self
+            .connection
+            .put(b"app_state:epoch", epoch.to_be_bytes())?)
     }
 }
 
