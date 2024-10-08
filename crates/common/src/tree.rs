60f9349db8906b17b5efd96e90b2bd1697a8a88d
Jonas Pusch <jonas@deltadevs.xyz>
2024-10-08 13:53:16 +0200

    docs: Add docs for HashchainResponse enum

--- crates/common/src/tree.rs
+++ crates/common/src/tree.rs
@@ -284,9 +284,13 @@ impl UpdateProof {
     }
 }
 
+/// Enumerates possible responses when fetching tree values
 #[derive(Debug)]
 pub enum HashchainResponse {
+    /// When a hashchain was found, provides the value and its corresponding membership-proof
     Found(Hashchain, MembershipProof),
+
+    /// When no hashchain was found for a specific key, provides the corresponding non-membership-proof
     NotFound(NonMembershipProof),
 }
 
