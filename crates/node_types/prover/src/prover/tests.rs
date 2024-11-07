e1d119341355190478585dcd725c2c5cfc41b4d9
Jonas <jonas@deltadevs.xyz>
2024-11-07 13:38:40 +0100

    test(prover): Prover tests use requests instead of operations

--- crates/node_types/prover/src/prover/tests.rs
+++ crates/node_types/prover/src/prover/tests.rs
@@ -1,9 +1,9 @@
 use super::*;
-use prism_common::{test_ops::OpsBuilder, tree::Proof};
+use prism_common::{test_ops::RequestBuilder, tree::Proof};
 use std::{self, sync::Arc, time::Duration};
 use tokio::spawn;
 
-use prism_common::{operation::Operation, test_utils::create_mock_signing_key};
+use prism_common::test_utils::create_mock_signing_key;
 use prism_da::memory::InMemoryDataAvailabilityLayer;
 use prism_storage::{inmemory::InMemoryDatabase, Database};
 
@@ -16,14 +16,14 @@ async fn create_test_prover() -> Arc<Prover> {
     Arc::new(Prover::new(db.clone(), da_layer, &cfg).unwrap())
 }
 
-fn create_mock_operations(service_id: String) -> Vec<Operation> {
-    let mut ops_builder = OpsBuilder::new();
+fn create_mock_requests(service_id: String) -> Vec<PendingRequest> {
+    let mut request_builder = RequestBuilder::new();
 
     vec![
-        ops_builder.register_service_with_random_key(&service_id).ex(),
-        ops_builder.create_account_with_random_key("user1@example.com", &service_id).ex(),
-        ops_builder.create_account_with_random_key("user2@example.com", &service_id).ex(),
-        ops_builder.add_random_key_verified_with_root("user1@example.com").ex(),
+        request_builder.register_service_with_random_keys(&service_id).ex(),
+        request_builder.create_account_with_random_key("user1@example.com", &service_id).ex(),
+        request_builder.create_account_with_random_key("user2@example.com", &service_id).ex(),
+        request_builder.add_random_key_verified_with_root("user1@example.com").ex(),
     ]
 }
 
@@ -31,50 +31,51 @@ fn create_mock_operations(service_id: String) -> Vec<Operation> {
 async fn test_validate_and_queue_update() {
     let prover = create_test_prover().await;
 
-    let mut ops_builder = OpsBuilder::new();
-    let op = ops_builder.register_service_with_random_key("test_service").ex();
+    let mut request_builder = RequestBuilder::new();
+    let request = request_builder.register_service_with_random_keys("test_service").ex();
 
-    prover.clone().validate_and_queue_update(&op).await.unwrap();
+    prover.clone().validate_and_queue_update(request.clone()).await.unwrap();
 
-    prover.clone().validate_and_queue_update(&op).await.unwrap();
+    prover.clone().validate_and_queue_update(request.clone()).await.unwrap();
 
-    let pending_ops = prover.pending_operations.read().await;
-    assert_eq!(pending_ops.len(), 2);
+    let pending_entries = prover.pending_requests.read().await;
+    assert_eq!(pending_entries.len(), 2);
 }
 
 #[tokio::test]
-async fn test_process_operation() {
+async fn test_process_entries() {
     let prover = create_test_prover().await;
 
-    let mut ops_builder = OpsBuilder::new();
-    let register_service_op = ops_builder.register_service_with_random_key("test_service").ex();
-    let create_account_op =
-        ops_builder.create_account_with_random_key("test_account", "test_service").ex();
+    let mut request_builder = RequestBuilder::new();
+    let register_service_request =
+        request_builder.register_service_with_random_keys("test_service").ex();
+    let create_account_request =
+        request_builder.create_account_with_random_key("test_account", "test_service").ex();
 
-    let proof = prover.process_operation(&register_service_op).await.unwrap();
+    let proof = prover.process_request(register_service_request).await.unwrap();
     assert!(matches!(proof, Proof::Insert(_)));
 
-    let proof = prover.process_operation(&create_account_op).await.unwrap();
+    let proof = prover.process_request(create_account_request.clone()).await.unwrap();
     assert!(matches!(proof, Proof::Insert(_)));
 
     let new_key = create_mock_signing_key();
-    let add_key_op =
-        ops_builder.add_key_verified_with_root("test_account", new_key.verifying_key()).ex();
+    let add_key_request =
+        request_builder.add_key_verified_with_root("test_account", new_key.verifying_key()).ex();
 
-    let proof = prover.process_operation(&add_key_op).await.unwrap();
+    let proof = prover.process_request(add_key_request).await.unwrap();
 
     assert!(matches!(proof, Proof::Update(_)));
 
     // Revoke original key
-    let revoke_op = ops_builder
+    let revoke_request = request_builder
         .revoke_key(
             "test_account",
-            create_account_op.get_public_key().cloned().unwrap(),
+            create_account_request.entry.operation.get_public_key().cloned().unwrap(),
             &new_key,
             1,
         )
         .ex();
-    let proof = prover.process_operation(&revoke_op).await.unwrap();
+    let proof = prover.process_request(revoke_request).await.unwrap();
     assert!(matches!(proof, Proof::Update(_)));
 }
 
@@ -82,12 +83,14 @@ async fn test_process_operation() {
 async fn test_execute_block_with_invalid_tx() {
     let prover = create_test_prover().await;
 
-    let mut ops_builder = OpsBuilder::new();
+    let mut ops_builder = RequestBuilder::new();
 
     let new_key_1 = create_mock_signing_key();
 
+    debug!("CAN YOU SEE ME?");
+
     let operations = vec![
-        ops_builder.register_service_with_random_key("service_id").ex(),
+        ops_builder.register_service_with_random_keys("service_id").ex(),
         ops_builder.create_account_with_random_key("account_id", "service_id").ex(),
         // add new key, so it will be index = 1
         ops_builder.add_key_verified_with_root("account_id", new_key_1.verifying_key()).ex(),
@@ -106,7 +109,7 @@ async fn test_execute_block_with_invalid_tx() {
 async fn test_execute_block() {
     let prover = create_test_prover().await;
 
-    let operations = create_mock_operations("test_service".to_string());
+    let operations = create_mock_requests("test_service".to_string());
 
     let proofs = prover.execute_block(operations).await.unwrap();
     assert_eq!(proofs.len(), 4);
@@ -115,7 +118,7 @@ async fn test_execute_block() {
 #[tokio::test]
 async fn test_finalize_new_epoch() {
     let prover = create_test_prover().await;
-    let operations = create_mock_operations("test_service".to_string());
+    let operations = create_mock_requests("test_service".to_string());
 
     let prev_commitment = prover.get_commitment().await.unwrap();
     prover.finalize_new_epoch(0, operations).await.unwrap();
@@ -138,10 +141,10 @@ async fn test_restart_sync_from_scratch() {
         runner.run().await.unwrap();
     });
 
-    let operations = create_mock_operations("test_service".to_string());
+    let requests = create_mock_requests("test_service".to_string());
 
-    for op in operations {
-        prover.clone().validate_and_queue_update(&op).await.unwrap();
+    for request in requests {
+        prover.clone().validate_and_queue_update(request).await.unwrap();
         while let Ok(new_block) = brx.recv().await {
             if new_block.epoch.is_some() {
                 break;
@@ -181,10 +184,10 @@ async fn test_load_persisted_state() {
         runner.run().await.unwrap();
     });
 
-    let operations = create_mock_operations("test_service".to_string());
+    let requests = create_mock_requests("test_service".to_string());
 
-    for op in operations {
-        prover.clone().validate_and_queue_update(&op).await.unwrap();
+    for request in requests {
+        prover.clone().validate_and_queue_update(request).await.unwrap();
         while let Ok(new_block) = brx.recv().await {
             if new_block.epoch.is_some() {
                 break;
