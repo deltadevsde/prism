6a386db1888122d7559a35db4d505e66b14dd806
Jonas <jonas@deltadevs.xyz>
2024-11-07 13:22:42 +0100

    refactor(common): Hashchain provides methods to do operations

--- crates/common/src/hashchain.rs
+++ crates/common/src/hashchain.rs
@@ -1,16 +1,18 @@
-use anyhow::{anyhow, bail, Result};
-use jmt::KeyHash;
+use anyhow::{anyhow, bail, ensure, Result};
 use serde::{Deserialize, Serialize};
-use std::{
-    collections::HashSet,
-    ops::{Deref, DerefMut},
-};
+use std::ops::{Deref, DerefMut};
 
-use crate::{digest::Digest, hasher::Hasher, keys::VerifyingKey, operation::Operation};
+use crate::{
+    digest::Digest,
+    keys::{Signature, SigningKey, VerifyingKey},
+    operation::{
+        HashchainSignatureBundle, Operation, ServiceChallenge, ServiceChallengeInput,
+        SignatureBundle,
+    },
+};
 
 #[derive(Clone, Serialize, Deserialize, Debug, PartialEq)]
 pub struct Hashchain {
-    pub id: String,
     pub entries: Vec<HashchainEntry>,
 }
 
@@ -56,36 +58,40 @@ impl DerefMut for Hashchain {
 }
 
 impl Hashchain {
-    pub fn from_operation(operation: Operation) -> Result<Self> {
-        let mut hc = Hashchain::empty(operation.id());
-        hc.perform_operation(operation)?;
-        Ok(hc)
-    }
-
-    pub fn empty(id: String) -> Self {
+    pub fn empty() -> Self {
         Self {
-            id,
             entries: Vec::new(),
         }
+    }
+
+    pub fn from_entry(entry: HashchainEntry) -> Result<Self> {
+        let mut hc = Hashchain::empty();
+        hc.add_entry(entry)?;
+        Ok(hc)
     }
 
     pub fn get_key_at_index(&self, idx: usize) -> Result<&VerifyingKey> {
         self.entries
             .get(idx)
             .and_then(|entry| entry.operation.get_public_key())
-            .ok_or_else(|| anyhow!("No valid public key found at index {}", idx))
+            .ok_or_else(|| anyhow!("No public key found at index {}", idx))
     }
 
-    pub fn is_key_invalid(&self, key: VerifyingKey) -> bool {
-        self.iter()
-            .rev()
-            .find_map(|entry| match entry.operation.clone() {
-                Operation::RevokeKey(args) if args.value == key => Some(true),
-                Operation::AddKey(args) if args.value == key => Some(false),
-                Operation::CreateAccount(args) if args.value == key => Some(false),
-                _ => None,
-            })
-            .unwrap_or(true)
+    pub fn is_key_invalid(&self, key: &VerifyingKey) -> bool {
+        for entry in self.iter().rev() {
+            if let Some(entry_key) = entry.operation.get_public_key() {
+                if key.eq(entry_key) {
+                    match entry.operation {
+                        Operation::RevokeKey { .. } => return true,
+                        Operation::AddKey { .. }
+                        | Operation::CreateAccount { .. }
+                        | Operation::RegisterService { .. } => return false,
+                        _ => {}
+                    }
+                }
+            }
+        }
+        true
     }
 
     pub fn get(&self, idx: usize) -> &HashchainEntry {
@@ -96,101 +102,124 @@ impl Hashchain {
         self.last().map_or(Digest::zero(), |entry| entry.hash)
     }
 
-    fn push(&mut self, operation: Operation) -> Result<HashchainEntry> {
-        if operation.id() != self.id {
-            bail!("Operation ID does not match Hashchain ID");
-        }
+    pub fn add_entry(&mut self, entry: HashchainEntry) -> Result<()> {
+        self.validate_new_entry(&entry)?;
+        self.entries.push(entry);
+        Ok(())
+    }
 
-        let previous_hash = self.last_hash();
-
-        let entry = HashchainEntry::new(operation, previous_hash);
-        self.entries.push(entry.clone());
-
+    pub fn register_service(
+        &mut self,
+        id: String,
+        creation_gate: ServiceChallenge,
+        key: VerifyingKey,
+        signing_key: &SigningKey,
+    ) -> Result<HashchainEntry> {
+        let entry = HashchainEntry::new_register_service(id, creation_gate, key, signing_key);
+        self.add_entry(entry.clone())?;
         Ok(entry)
     }
 
-    pub fn perform_operation(&mut self, operation: Operation) -> Result<HashchainEntry> {
-        self.validate_new_operation(&operation)?;
-        self.push(operation)
+    pub fn create_account(
+        &mut self,
+        id: String,
+        service_id: String,
+        challenge: ServiceChallengeInput,
+        key: VerifyingKey,
+        signing_key: &SigningKey,
+    ) -> Result<HashchainEntry> {
+        let entry = HashchainEntry::new_create_account(id, service_id, challenge, key, signing_key);
+        self.add_entry(entry.clone())?;
+        Ok(entry)
     }
 
-    /// Verifies the structure and signature of a new operation
-    fn validate_new_operation(&self, operation: &Operation) -> Result<()> {
-        match operation {
-            Operation::RegisterService(args) => {
+    pub fn add_key(
+        &mut self,
+        key: VerifyingKey,
+        signing_key: &SigningKey,
+        key_idx: usize,
+    ) -> Result<HashchainEntry> {
+        let entry = HashchainEntry::new_add_key(key, self.last_hash(), signing_key, key_idx);
+        self.add_entry(entry.clone())?;
+        Ok(entry)
+    }
+
+    pub fn revoke_key(
+        &mut self,
+        key: VerifyingKey,
+        signing_key: &SigningKey,
+        key_idx: usize,
+    ) -> Result<HashchainEntry> {
+        let entry = HashchainEntry::new_revoke_key(key, self.last_hash(), signing_key, key_idx);
+        self.add_entry(entry.clone())?;
+        Ok(entry)
+    }
+
+    pub fn add_data(
+        &mut self,
+        data: Vec<u8>,
+        data_signature: Option<SignatureBundle>,
+        signing_key: &SigningKey,
+        key_idx: usize,
+    ) -> Result<HashchainEntry> {
+        let entry = HashchainEntry::new_add_data(
+            data,
+            data_signature,
+            self.last_hash(),
+            signing_key,
+            key_idx,
+        );
+        self.add_entry(entry.clone())?;
+        Ok(entry)
+    }
+
+    fn validate_new_entry(&self, entry: &HashchainEntry) -> Result<()> {
+        entry.validate_operation()?;
+
+        let last_hash = self.last_hash();
+        if entry.previous_hash != last_hash {
+            bail!(
+                "Previous hash for key operation must be the last hash - prev: {}, last: {}",
+                entry.previous_hash,
+                last_hash
+            )
+        }
+
+        let verifying_key = self.verifying_key_for_entry(entry)?;
+
+        match entry.operation {
+            Operation::CreateAccount { .. } | Operation::RegisterService { .. } => {
                 if !self.entries.is_empty() {
-                    bail!("RegisterService operation must be the first entry");
+                    bail!("Operation {} must be the first entry", &entry.operation);
                 }
-
-                if args.prev_hash != Digest::zero() {
-                    bail!(
-                        "Previous hash for initial operation must be zero, but was {}",
-                        args.prev_hash
-                    )
-                }
-
-                Ok(())
             }
-            Operation::AddKey(args) | Operation::RevokeKey(args) => {
-                let last_hash = self.last_hash();
-                if args.prev_hash != last_hash {
-                    bail!(
-                        "Previous hash for key operation must be the last hash - prev: {}, last: {}",
-                        args.prev_hash,
-                        last_hash
-                    )
+            Operation::AddData { .. } | Operation::AddKey { .. } | Operation::RevokeKey { .. } => {
+                if self.entries.is_empty() {
+                    bail!("Operation {} must not be the first entry", &entry.operation);
                 }
 
-                let key_idx = args.signature.key_idx;
-                let verifying_key = self.get_key_at_index(key_idx)?;
-
-                if self.is_key_invalid(verifying_key.clone()) {
-                    bail!(
-                        "The key at index {}, intended to verify this operation, is invalid",
-                        key_idx
-                    );
+                if self.is_key_invalid(verifying_key) {
+                    bail!("Invalid key at index {}", &entry.signature_bundle.key_idx);
                 }
-
-                operation.verify_user_signature(verifying_key)
-            }
-            Operation::AddData(args) => {
-                let last_hash = self.last_hash();
-                if args.prev_hash != last_hash {
-                    bail!(
-                        "Previous hash for add-data operation is not equal to the last hash - prev: {}, last: {}",
-                        args.prev_hash,
-                        last_hash
-                    )
-                }
-
-                let key_idx = args.op_signature.key_idx;
-                let verifying_key = self.get_key_at_index(key_idx)?;
-
-                if self.is_key_invalid(verifying_key.clone()) {
-                    bail!(
-                        "The key at index {}, intended to verify this operation, is invalid",
-                        key_idx
-                    );
-                }
-
-                operation.verify_user_signature(verifying_key)
-            }
-            Operation::CreateAccount(args) => {
-                if !self.entries.is_empty() {
-                    bail!("CreateAccount operation must be the first entry");
-                }
-
-                if args.prev_hash != Digest::zero() {
-                    bail!("Previous hash for initial operation must be zero")
-                }
-
-                operation.verify_user_signature(&args.value)
             }
         }
+
+        entry.validate_hash()?;
+        entry.validate_signature(verifying_key)
     }
 
-    pub fn get_keyhash(&self) -> KeyHash {
-        KeyHash::with::<Hasher>(Digest::hash(self.id.clone()))
+    fn verifying_key_for_entry<'a>(
+        &'a self,
+        entry: &'a HashchainEntry,
+    ) -> Result<&'a VerifyingKey> {
+        match &entry.operation {
+            Operation::CreateAccount { key, .. } | Operation::RegisterService { key, .. } => {
+                Ok(key)
+            }
+            Operation::AddData { .. } | Operation::AddKey { .. } | Operation::RevokeKey { .. } => {
+                self.get_key_at_index(entry.signature_bundle.key_idx)
+            }
+        }
     }
 
     pub fn is_empty(&self) -> bool {
@@ -209,19 +238,146 @@ pub struct HashchainEntry {
     pub hash: Digest,
     pub previous_hash: Digest,
     pub operation: Operation,
+    pub signature_bundle: HashchainSignatureBundle,
 }
 
 impl HashchainEntry {
-    pub fn new(operation: Operation, previous_hash: Digest) -> Self {
-        let mut data = Vec::new();
-        data.extend_from_slice(operation.to_string().as_bytes());
-        data.extend_from_slice(previous_hash.as_ref());
-        let hash = Digest::hash(data);
+    pub fn new(
+        operation: Operation,
+        previous_hash: Digest,
+        signing_key: &SigningKey,
+        key_idx: usize,
+    ) -> Self {
+        let serialized_operation =
+            bincode::serialize(&operation).expect("Serializing operation should work");
+        let hash =
+            Digest::hash_items(&[serialized_operation.as_slice(), &previous_hash.to_bytes()]);
+
+        let signature_bundle = HashchainSignatureBundle {
+            signature: signing_key.sign(hash.as_ref()),
+            key_idx,
+        };
 
         Self {
             hash,
             previous_hash,
             operation,
+            signature_bundle,
+        }
+    }
+
+    pub fn new_genesis(operation: Operation, signing_key: &SigningKey) -> Self {
+        Self::new(operation, Digest::zero(), signing_key, 0)
+    }
+
+    pub fn new_register_service(
+        id: String,
+        creation_gate: ServiceChallenge,
+        key: VerifyingKey,
+        signing_key: &SigningKey,
+    ) -> Self {
+        let operation = Operation::RegisterService {
+            id,
+            creation_gate,
+            key,
+        };
+        Self::new_genesis(operation, signing_key)
+    }
+
+    pub fn new_create_account(
+        id: String,
+        service_id: String,
+        challenge: ServiceChallengeInput,
+        key: VerifyingKey,
+        signing_key: &SigningKey,
+    ) -> Self {
+        let operation = Operation::CreateAccount {
+            id,
+            service_id,
+            challenge,
+            key,
+        };
+        Self::new_genesis(operation, signing_key)
+    }
+
+    pub fn new_add_key(
+        key: VerifyingKey,
+        prev_hash: Digest,
+        signing_key: &SigningKey,
+        key_idx: usize,
+    ) -> Self {
+        let operation = Operation::AddKey { key };
+        Self::new(operation, prev_hash, signing_key, key_idx)
+    }
+
+    pub fn new_revoke_key(
+        key: VerifyingKey,
+        prev_hash: Digest,
+        signing_key: &SigningKey,
+        key_idx: usize,
+    ) -> Self {
+        let operation = Operation::RevokeKey { key };
+        Self::new(operation, prev_hash, signing_key, key_idx)
+    }
+
+    pub fn new_add_data(
+        data: Vec<u8>,
+        data_signature: Option<SignatureBundle>,
+        prev_hash: Digest,
+        signing_key: &SigningKey,
+        key_idx: usize,
+    ) -> Self {
+        let operation = Operation::AddData {
+            data,
+            data_signature,
+        };
+        Self::new(operation, prev_hash, signing_key, key_idx)
+    }
+
+    pub fn validate_hash(&self) -> Result<()> {
+        // let pristine_entry = self.without_challenge().without_signature();
+        let pristine_entry = self.without_signature();
+
+        let serialized_operation = bincode::serialize(&pristine_entry.operation)
+            .expect("Serializing operation should work");
+        let pristine_entry_hash = Digest::hash_items(&[
+            serialized_operation.as_slice(),
+            &pristine_entry.previous_hash.to_bytes(),
+        ]);
+
+        ensure!(
+            self.hash == pristine_entry_hash,
+            "Hashchain entry has incorrect hash"
+        );
+        Ok(())
+    }
+
+    pub fn validate_signature(&self, verifying_key: &VerifyingKey) -> Result<()> {
+        verifying_key.verify_signature(self.hash.as_ref(), &self.signature_bundle.signature)?;
+        Ok(())
+    }
+
+    pub fn validate_operation(&self) -> Result<()> {
+        self.operation.validate_basic()
+    }
+
+    pub fn without_signature(&self) -> Self {
+        Self {
+            signature_bundle: HashchainSignatureBundle {
+                key_idx: self.signature_bundle.key_idx,
+                signature: Signature::Placeholder,
+            },
+            ..self.clone()
+        }
+    }
+
+    pub fn without_challenge(&self) -> Self {
+        match self.operation {
+            Operation::CreateAccount { .. } => Self {
+                operation: self.operation.without_challenge(),
+                ..self.clone()
+            },
+            _ => self.clone(),
         }
     }
 }
