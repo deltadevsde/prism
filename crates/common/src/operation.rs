a5cfe78352cf20be657f1efaee5461de732f73a4
Jonas <jonas@deltadevs.xyz>
2024-11-07 13:18:03 +0100

    refactor(common): Operation is only a container for op-specific args

--- crates/common/src/operation.rs
+++ crates/common/src/operation.rs
@@ -1,29 +1,36 @@
-use anyhow::{Context, Result};
-use bincode;
-use celestia_types::Blob;
-use prism_errors::GeneralError;
+use anyhow::{bail, ensure, Result};
+
 use serde::{Deserialize, Serialize};
 use std::{self, fmt::Display};
 
-use crate::{
-    digest::Digest,
-    keys::{Signature, SigningKey, VerifyingKey},
-};
+use crate::keys::{Signature, SigningKey, VerifyingKey};
 
 #[derive(Clone, Serialize, Deserialize, Debug, PartialEq)]
 /// An [`Operation`] represents a state transition in the system.
 /// In a blockchain analogy, this would be the full set of our transaction types.
 pub enum Operation {
-    /// Creates a new account with the given id and value.
-    CreateAccount(CreateAccountArgs),
-    /// Adds a key to an existing account.
-    AddKey(KeyOperationArgs),
-    /// Adds arbitrary signed data to an existing account.
-    AddData(AddDataArgs),
-    /// Revokes a key from an existing account.
-    RevokeKey(KeyOperationArgs),
+    /// Creates a new account with the given id and key.
+    CreateAccount {
+        id: String,
+        service_id: String,
+        challenge: ServiceChallengeInput,
+        key: VerifyingKey,
+    },
     /// Registers a new service with the given id.
-    RegisterService(RegisterServiceArgs),
+    RegisterService {
+        id: String,
+        creation_gate: ServiceChallenge,
+        key: VerifyingKey,
+    },
+    /// Adds arbitrary signed data to an existing account.
+    AddData {
+        data: Vec<u8>,
+        data_signature: Option<SignatureBundle>,
+    },
+    /// Adds a key to an existing account.
+    AddKey { key: VerifyingKey },
+    /// Revokes a key from an existing account.
+    RevokeKey { key: VerifyingKey },
 }
 
 #[derive(Clone, Serialize, Deserialize, Default, Debug, PartialEq)]
@@ -62,34 +69,6 @@ pub enum ServiceChallengeInput {
 }
 
 #[derive(Clone, Serialize, Deserialize, Debug, PartialEq)]
-/// Arguments for creating an account with a service.
-pub struct CreateAccountArgs {
-    /// Account ID
-    pub id: String,
-    /// Public key being added
-    pub value: VerifyingKey,
-    /// Associated service ID
-    pub service_id: String,
-    /// Challenge input for verification
-    pub challenge: ServiceChallengeInput,
-    /// The hash of the previous operation
-    pub prev_hash: Digest,
-    /// The signature that signed the operation
-    pub signature: Signature,
-}
-
-#[derive(Clone, Serialize, Deserialize, Debug, PartialEq)]
-/// Arguments for registering a new service.
-pub struct RegisterServiceArgs {
-    /// Service ID
-    pub id: String,
-    /// Challenge gate for access control
-    pub creation_gate: ServiceChallenge,
-    /// The hash of the previous operation
-    pub prev_hash: Digest,
-}
-
-#[derive(Clone, Serialize, Deserialize, Debug, PartialEq)]
 pub enum ServiceChallenge {
     Signed(VerifyingKey),
 }
@@ -100,299 +79,53 @@ impl From<SigningKey> for ServiceChallenge {
     }
 }
 
-#[derive(Clone, Serialize, Deserialize, Debug, PartialEq)]
-/// Structure for adding data.
-pub struct AddDataArgs {
-    /// Account ID
-    pub id: String,
-    /// Data to be added
-    pub value: Vec<u8>,
-    /// Optional external signature used to sign the data to be added
-    pub value_signature: Option<SignatureBundle>,
-    /// The hash of the previous operation
-    pub prev_hash: Digest,
-    /// Signature to authorize the action
-    pub op_signature: HashchainSignatureBundle,
-}
-
-#[derive(Clone, Serialize, Deserialize, Debug, PartialEq)]
-/// Common structure for operations involving keys (adding or revoking).
-pub struct KeyOperationArgs {
-    /// Account ID
-    pub id: String,
-    /// Public key being added or revoked
-    pub value: VerifyingKey,
-    /// The hash of the previous operation
-    pub prev_hash: Digest,
-    /// Signature to authorize the action
-    pub signature: HashchainSignatureBundle,
-}
-
 impl Operation {
-    pub fn new_create_account(
-        id: String,
-        signing_key: &SigningKey,
-        service_id: String,
-        service_signer: &SigningKey,
-    ) -> Result<Self> {
-        let mut op = Operation::CreateAccount(CreateAccountArgs {
-            id: id.to_string(),
-            value: signing_key.clone().verifying_key(),
-            service_id,
-            challenge: ServiceChallengeInput::Signed(Signature::Placeholder),
-            prev_hash: Digest::zero(),
-            signature: Signature::Placeholder,
-        });
-
-        op.insert_signature(signing_key)
-            .expect("Inserting signature into operation should succeed");
-
-        let msg = bincode::serialize(&op).unwrap();
-        let service_challenge = service_signer.sign(&msg);
-
-        match op {
-            Operation::CreateAccount(ref mut args) => {
-                args.challenge = ServiceChallengeInput::Signed(service_challenge);
-            }
-            _ => panic!("Operation should be CreateAccount"),
-        };
-        Ok(op)
-    }
-
-    pub fn new_register_service(id: String, creation_gate: ServiceChallenge) -> Self {
-        Operation::RegisterService(RegisterServiceArgs {
-            id,
-            creation_gate,
-            prev_hash: Digest::zero(),
-        })
-    }
-
-    pub fn new_add_key(
-        id: String,
-        value: VerifyingKey,
-        prev_hash: Digest,
-        signing_key: &SigningKey,
-        key_idx: usize,
-    ) -> Result<Self> {
-        let op_to_sign = Operation::AddKey(KeyOperationArgs {
-            id: id.clone(),
-            value: value.clone(),
-            prev_hash,
-            signature: HashchainSignatureBundle::empty_with_idx(key_idx),
-        });
-
-        let message = bincode::serialize(&op_to_sign)?;
-        let signature = HashchainSignatureBundle {
-            key_idx,
-            signature: signing_key.sign(&message),
-        };
-
-        Ok(Operation::AddKey(KeyOperationArgs {
-            id,
-            value,
-            prev_hash,
-            signature,
-        }))
-    }
-
-    pub fn new_revoke_key(
-        id: String,
-        value: VerifyingKey,
-        prev_hash: Digest,
-        signing_key: &SigningKey,
-        key_idx: usize,
-    ) -> Result<Self> {
-        let op_to_sign = Operation::RevokeKey(KeyOperationArgs {
-            id: id.clone(),
-            value: value.clone(),
-            prev_hash,
-            signature: HashchainSignatureBundle::empty_with_idx(key_idx),
-        });
-
-        let message = bincode::serialize(&op_to_sign)?;
-        let signature = HashchainSignatureBundle {
-            key_idx,
-            signature: signing_key.sign(&message),
-        };
-
-        Ok(Operation::RevokeKey(KeyOperationArgs {
-            id,
-            value,
-            prev_hash,
-            signature,
-        }))
-    }
-
-    pub fn new_add_signed_data(
-        id: String,
-        value: Vec<u8>,
-        value_signature: Option<SignatureBundle>,
-        prev_hash: Digest,
-        signing_key: &SigningKey,
-        key_idx: usize,
-    ) -> Result<Self> {
-        let op_to_sign = Operation::AddData(AddDataArgs {
-            id: id.clone(),
-            value: value.clone(),
-            value_signature: value_signature.clone(),
-            prev_hash,
-            op_signature: HashchainSignatureBundle::empty_with_idx(key_idx),
-        });
-
-        let message = { bincode::serialize(&op_to_sign)? };
-        let op_signature = HashchainSignatureBundle {
-            key_idx,
-            signature: signing_key.sign(&message),
-        };
-
-        Ok(Operation::AddData(AddDataArgs {
-            id,
-            value,
-            value_signature,
-            prev_hash,
-            op_signature,
-        }))
-    }
-
-    pub fn id(&self) -> String {
-        match self {
-            Operation::CreateAccount(args) => args.id.clone(),
-            Operation::AddKey(args) | Operation::RevokeKey(args) => args.id.clone(),
-            Operation::AddData(args) => args.id.clone(),
-            Operation::RegisterService(args) => args.id.clone(),
-        }
-    }
-
     pub fn get_public_key(&self) -> Option<&VerifyingKey> {
         match self {
-            Operation::RevokeKey(args) | Operation::AddKey(args) => Some(&args.value),
-            Operation::CreateAccount(args) => Some(&args.value),
-            Operation::RegisterService(_) | Operation::AddData(_) => None,
+            Operation::RevokeKey { key }
+            | Operation::AddKey { key }
+            | Operation::CreateAccount { key, .. }
+            | Operation::RegisterService { key, .. } => Some(key),
+            Operation::AddData { .. } => None,
         }
-    }
-
-    pub fn insert_signature(&mut self, signing_key: &SigningKey) -> Result<()> {
-        let serialized = bincode::serialize(self).context("Failed to serialize operation")?;
-        let signature = signing_key.sign(&serialized);
-
-        match self {
-            Operation::CreateAccount(args) => args.signature = signature,
-            Operation::AddKey(args) | Operation::RevokeKey(args) => {
-                args.signature.signature = signature
-            }
-            _ => unimplemented!("RegisterService prover gating not yet implemented"),
-        }
-        Ok(())
     }
 
     pub fn without_challenge(&self) -> Self {
-        match self {
-            Operation::CreateAccount(args) => Operation::CreateAccount(CreateAccountArgs {
-                id: args.id.clone(),
-                value: args.value.clone(),
-                service_id: args.service_id.clone(),
-                challenge: ServiceChallengeInput::Signed(Signature::Placeholder),
-                prev_hash: args.prev_hash,
-                signature: args.signature.clone(),
-            }),
-            _ => self.clone(),
+        let Operation::CreateAccount {
+            id,
+            service_id,
+            key,
+            ..
+        } = self
+        else {
+            return self.clone();
+        };
+
+        Operation::CreateAccount {
+            id: id.clone(),
+            service_id: service_id.clone(),
+            key: key.clone(),
+            challenge: ServiceChallengeInput::Signed(Signature::Placeholder),
         }
     }
 
-    pub fn without_signature(&self) -> Self {
-        match self {
-            Operation::AddKey(args) => Operation::AddKey(KeyOperationArgs {
-                id: args.id.clone(),
-                value: args.value.clone(),
-                prev_hash: args.prev_hash,
-                signature: HashchainSignatureBundle {
-                    key_idx: args.signature.key_idx,
-                    signature: Signature::Placeholder,
-                },
-            }),
-            Operation::RevokeKey(args) => Operation::RevokeKey(KeyOperationArgs {
-                id: args.id.clone(),
-                value: args.value.clone(),
-                prev_hash: args.prev_hash,
-                signature: HashchainSignatureBundle {
-                    key_idx: args.signature.key_idx,
-                    signature: Signature::Placeholder,
-                },
-            }),
-            Operation::AddData(args) => Operation::AddData(AddDataArgs {
-                id: args.id.clone(),
-                value: args.value.clone(),
-                prev_hash: args.prev_hash,
-                value_signature: args.value_signature.clone(),
-                op_signature: HashchainSignatureBundle {
-                    key_idx: args.op_signature.key_idx,
-                    signature: Signature::Placeholder,
-                },
-            }),
-            Operation::CreateAccount(args) => Operation::CreateAccount(CreateAccountArgs {
-                id: args.id.clone(),
-                value: args.value.clone(),
-                service_id: args.service_id.clone(),
-                challenge: args.challenge.clone(),
-                prev_hash: args.prev_hash,
-                signature: Signature::Placeholder,
-            }),
-            Operation::RegisterService(args) => Operation::RegisterService(RegisterServiceArgs {
-                id: args.id.clone(),
-                creation_gate: args.creation_gate.clone(),
-                prev_hash: args.prev_hash,
-            }),
-        }
-    }
-
-    pub fn verify_user_signature(&self, pubkey: &VerifyingKey) -> Result<()> {
-        match self {
-            Operation::RegisterService(_) => Ok(()),
-            Operation::CreateAccount(args) => {
-                let message = bincode::serialize(&self.without_signature().without_challenge())
-                    .context("User signature failed")?;
-                args.value.verify_signature(&message, &args.signature)
-            }
-            Operation::AddKey(args) | Operation::RevokeKey(args) => {
-                let message = bincode::serialize(&self.without_signature())
-                    .context("User signature failed")?;
-                pubkey.verify_signature(&message, &args.signature.signature)
-            }
-            Operation::AddData(args) => {
-                let message = bincode::serialize(&self.without_signature())
-                    .context("Serializing operation failed")?;
-                pubkey
-                    .verify_signature(&message, &args.op_signature.signature)
-                    .context("Verifying operation signature failed")?;
-
-                let Some(value_signature) = &args.value_signature else {
-                    return Ok(());
-                };
-
-                // If data to be added is signed, also validate its signature
-                value_signature
-                    .verifying_key
-                    .verify_signature(&args.value, &value_signature.signature)
-                    .context("Verifying value signature failed")
-            }
-        }
-    }
-
-    pub fn validate(&self) -> Result<()> {
+    pub fn validate_basic(&self) -> Result<()> {
         match &self {
-            Operation::AddKey(KeyOperationArgs { id, .. })
-            | Operation::RevokeKey(KeyOperationArgs { id, .. })
-            | Operation::AddData(AddDataArgs { id, .. })
-            | Operation::CreateAccount(CreateAccountArgs { id, .. }) => {
-                if id.is_empty() {
-                    return Err(
-                        GeneralError::MissingArgumentError("id is empty".to_string()).into(),
-                    );
+            Operation::RegisterService { .. } => Ok(()),
+            Operation::CreateAccount { service_id, .. } => {
+                if service_id.is_empty() {
+                    bail!("service_id must not be empty when adding service");
                 }
 
                 Ok(())
             }
-            Operation::RegisterService(_) => Ok(()),
+            Operation::AddKey { .. } | Operation::RevokeKey { .. } => Ok(()),
+            Operation::AddData { data, .. } => {
+                let data_len = data.len();
+                // TODO determine proper max data size here
+                ensure!(data_len < usize::MAX, "Incoming data size is {}", data_len);
+                Ok(())
+            }
         }
     }
 }
@@ -401,14 +134,5 @@ impl Display for Operation {
     // just print the debug
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{:?}", self)
-    }
-}
-
-impl TryFrom<&Blob> for Operation {
-    type Error = anyhow::Error;
-
-    fn try_from(value: &Blob) -> Result<Self, Self::Error> {
-        bincode::deserialize(&value.data)
-            .context(format!("Failed to decode blob into Operation: {value:?}"))
     }
 }
