072bdaef0c138632b6561df40cb6bb3d5110d270
Jonas <jonas@deltadevs.xyz>
2024-12-09 13:57:49 +0100

    feature(prover): commitment REST response is wrapped into an object
    Using JSON here too, in order not to have different content types for the response. Also, wrapping it into an object is considered a safe practice against some kinds of code injection attacks.

--- crates/node_types/prover/src/webserver.rs
+++ crates/node_types/prover/src/webserver.rs
@@ -60,8 +60,10 @@ pub struct TransactionRequest {
 #[derive(Serialize, Deserialize, ToSchema)]
 pub struct UpdateProofResponse(UpdateProof);
 
-#[derive(Serialize, Deserialize, ToSchema)]
-pub struct Hash(Digest);
+#[derive(Serialize, ToSchema)]
+pub struct CommitmentResponse {
+    commitment: Digest,
+}
 
 #[derive(Serialize, Deserialize, ToSchema)]
 pub struct UserKeyRequest {
@@ -81,7 +83,7 @@ pub struct UserKeyResponse {
         TransactionRequest,
         EpochData,
         UpdateProofResponse,
-        Hash,
+        CommitmentResponse,
         UserKeyRequest,
         UserKeyResponse
     ))
@@ -211,13 +213,13 @@ async fn get_hashchain(
     get,
     path = "/get-current-commitment",
     responses(
-        (status = 200, description = "Successfully retrieved current commitment", body = Hash),
+        (status = 200, description = "Successfully retrieved current commitment", body = CommitmentResponse),
         (status = 500, description = "Internal server error")
     )
 )]
 async fn get_commitment(State(session): State<Arc<Prover>>) -> impl IntoResponse {
     match session.get_commitment().await {
-        Ok(commitment) => (StatusCode::OK, Json(commitment)).into_response(),
+        Ok(commitment) => (StatusCode::OK, Json(CommitmentResponse { commitment })).into_response(),
         Err(e) => (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()).into_response(),
     }
 }
