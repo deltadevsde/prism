bc5fd5be541e45db7a4eb3129980bf334afccc24
Jonas <jonas@deltadevs.xyz>
2024-12-11 15:01:41 +0100

    feature: ensure correct length when decoding base64

--- crates/serde/src/base64.rs
+++ crates/serde/src/base64.rs
@@ -1,5 +1,5 @@
-use anyhow::Result;
-use base64::{engine::general_purpose::STANDARD as BASE64, Engine};
+use anyhow::{bail, Result};
+use base64::{decoded_len_estimate, engine::general_purpose::STANDARD as BASE64, Engine};
 
 pub trait ToBase64 {
     fn to_base64(&self) -> String;
@@ -32,6 +32,11 @@ impl FromBase64 for [u8; 32] {
     type Error = anyhow::Error;
 
     fn from_base64<T: AsRef<[u8]>>(base64: T) -> Result<Self> {
+        let decoded_len = decoded_len_estimate(base64.as_ref().len());
+        if decoded_len != 32 {
+            bail!("Expected decoded length to be 32, but was {decoded_len}");
+        }
+
         let mut output = [0u8; 32];
         BASE64.decode_slice(base64, &mut output)?;
         Ok(output)
