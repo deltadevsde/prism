4fcfd0b7a516c4dbadc0c9aae49097cd050fb3a8
Jonas <jonas@deltadevs.xyz>
2024-12-11 13:31:20 +0100

    build: delegate bytes serialization to serde_bytes lib

--- crates/serde/src/lib.rs
+++ crates/serde/src/lib.rs
@@ -14,66 +14,69 @@ pub struct CryptoPayload {
 pub mod raw_or_hex {
     use std::fmt::Display;
 
-    use serde::{self, Deserialize, Deserializer, Serializer};
-
     use crate::hex::{FromHex, ToHex};
+    use serde::{self, Deserialize, Deserializer, Serializer};
+    use serde_bytes::{Deserialize as BytesDeserialize, Serialize as BytesSerialize};
 
     pub fn serialize<S, T>(encodable: T, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
-        T: ToHex + AsRef<[u8]>,
+        T: ToHex + BytesSerialize,
     {
         if serializer.is_human_readable() {
             let hex_str = encodable.to_hex();
             serializer.serialize_str(&hex_str)
         } else {
-            serializer.serialize_bytes(encodable.as_ref())
+            encodable.serialize(serializer)
         }
     }
 
     pub fn deserialize<'de, D, T>(deserializer: D) -> Result<T, D::Error>
     where
         D: Deserializer<'de>,
-        T: FromHex + Deserialize<'de>,
+        T: FromHex + BytesDeserialize<'de>,
         T::Error: Display,
     {
         if deserializer.is_human_readable() {
             let hex_str = String::deserialize(deserializer)?;
             T::from_hex(hex_str).map_err(serde::de::Error::custom)
         } else {
-            Deserialize::deserialize(deserializer)
+            T::deserialize(deserializer)
         }
     }
 }
 
 pub mod raw_or_b64 {
-    use crate::base64::{FromBase64, ToBase64};
-    use serde::{self, Deserialize, Deserializer, Serializer};
     use std::fmt::Display;
 
-    pub fn serialize<S, T: AsRef<[u8]>>(bytes: T, serializer: S) -> Result<S::Ok, S::Error>
+    use crate::base64::{FromBase64, ToBase64};
+    use serde::{self, Deserialize, Deserializer, Serializer};
+    use serde_bytes::{Deserialize as BytesDeserialize, Serialize as BytesSerialize};
+
+    pub fn serialize<S, T>(encodable: T, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
+        T: ToBase64 + BytesSerialize,
     {
         if serializer.is_human_readable() {
-            let base64_string = bytes.to_base64();
+            let base64_string = encodable.to_base64();
             serializer.serialize_str(&base64_string)
         } else {
-            serializer.serialize_bytes(bytes.as_ref())
+            encodable.serialize(serializer)
         }
     }
 
     pub fn deserialize<'de, D, T>(deserializer: D) -> Result<T, D::Error>
     where
         D: Deserializer<'de>,
-        T: FromBase64 + Deserialize<'de>,
+        T: FromBase64 + BytesDeserialize<'de>,
         T::Error: Display,
     {
         if deserializer.is_human_readable() {
             let base64_string = String::deserialize(deserializer)?;
             T::from_base64(base64_string).map_err(serde::de::Error::custom)
         } else {
-            Deserialize::deserialize(deserializer)
+            T::deserialize(deserializer)
         }
     }
 }
