d61f0f19a76d30c3d272dd4ee2e077fb7b59f626
Ryan Quinn Ford <ryan@deltadevs.xyz>
2024-12-20 11:47:01 +0100

    fix: verifying service challenges in InsertProof

--- crates/zk/sp1/src/main.rs
+++ crates/zk/sp1/src/main.rs
@@ -1,27 +1,11 @@
 #![no_main]
 sp1_zkvm::entrypoint!(main);
 
-use prism_common::digest::Digest;
-use prism_tree::proofs::{Batch, Proof};
+use prism_tree::proofs::Batch;
 
 pub fn main() {
     let batch = sp1_zkvm::io::read::<Batch>();
-    let mut current = batch.prev_root;
-    sp1_zkvm::io::commit_slice(&current.0);
-
-    for proof in batch.proofs.iter() {
-        match proof {
-            Proof::Update(p) => {
-                assert_eq!(current, Digest::new(p.old_root.0));
-                assert!(p.verify().is_ok());
-                current = Digest::new(p.new_root.0);
-            }
-            Proof::Insert(p) => {
-                assert_eq!(current, Digest::new(p.non_membership_proof.root.0));
-                assert!(p.verify().is_ok());
-                current = p.new_root;
-            }
-        }
-    }
-    sp1_zkvm::io::commit_slice(&current.0);
+    sp1_zkvm::io::commit_slice(&batch.prev_root.0);
+    batch.verify().unwrap();
+    sp1_zkvm::io::commit_slice(&batch.new_root.0);
 }
