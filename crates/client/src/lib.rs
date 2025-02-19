3735ce034fc97d9c6dccac6c836bf340ae6d203f
Jonas <jonas@deltadevs.xyz>
2025-02-19 14:40:52 +0100

    feat(client): expose more types to users

--- crates/client/src/lib.rs
+++ crates/client/src/lib.rs
@@ -4,9 +4,11 @@ mod timer;
 pub use http_client::{PrismHttpClient, PrismHttpClientError};
 pub use prism_common::{
     account::Account,
-    api::{PendingTransaction, PrismApi},
+    api::{types::*, PendingTransaction, PrismApi},
     builder,
     digest::Digest,
-    operation::{ServiceChallenge, ServiceChallengeInput},
+    operation::{ServiceChallenge, ServiceChallengeInput, SignatureBundle},
+    transaction::{Transaction, TransactionError, UnsignedTransaction},
 };
 pub use prism_keys::{Signature, SigningKey, VerifyingKey};
+pub use prism_serde::binary;
