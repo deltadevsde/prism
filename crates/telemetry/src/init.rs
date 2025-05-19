55f516b2b801167b1ac0b3528402e661baae7bb5
Smuu <18609909+Smuu@users.noreply.github.com>
2025-05-19 16:32:16 +0200

    feat: refactor telemetry
    Signed-off-by: Smuu <18609909+Smuu@users.noreply.github.com>

---
+++ crates/telemetry/src/init.rs
@@ -0,0 +1,39 @@
+use std::io::Error;
+
+use crate::metrics_registry::init_metrics_registry;
+
+use opentelemetry::global::{self};
+use opentelemetry_sdk::logs::SdkLoggerProvider;
+use opentelemetry_sdk::metrics::SdkMeterProvider;
+use prism_telemetry::telemetry::{init_telemetry, build_resource, set_global_attributes};
+use prism_telemetry::logs::setup_log_subscriber;
+use prism_telemetry::config::TelemetryConfig;
+
+pub fn init(telemetry_config: TelemetryConfig, attributes: Vec<(String, String)>) -> Result<(Option<SdkMeterProvider>, Option<SdkLoggerProvider>), Error> {
+    // Initialize the telemetry system
+
+    let mut attributes = attributes.clone();
+    attributes.extend(telemetry_config.global_labels.labels.clone());
+
+    set_global_attributes(attributes.clone());
+
+    let resource = build_resource("prism-messenger-server".to_string(), attributes);
+
+        let (meter_provider, log_provider) = init_telemetry(&telemetry_config, resource).map_err(|e| Error::other(e.to_string()))?;
+
+    if let Some(ref provider) = meter_provider {
+        global::set_meter_provider(provider.clone());
+
+        // Initialize the metrics registry after setting the global meter provider
+        init_metrics_registry();
+    }
+
+    if let Some(ref provider) = log_provider {
+        // Initialize tracing subscriber
+        setup_log_subscriber(
+            telemetry_config.logs.enabled,
+            Some(provider)
+        );
+    }
+    Ok((meter_provider, log_provider))
+}
