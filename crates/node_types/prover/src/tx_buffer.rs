6ab2b7fbc24ec50c7f47e4cba990446ac40e7bfd
sebasti810 <spusch@outlook.de>
2025-05-30 11:50:26 +0200

    refac: TxBuffer as a struct

--- crates/node_types/prover/src/tx_buffer.rs
+++ crates/node_types/prover/src/tx_buffer.rs
@@ -1,30 +1,35 @@
+use prism_common::transaction::Transaction;
 use std::collections::BTreeMap;
 
-use prism_common::transaction::Transaction;
-
-pub struct TxBuffer(BTreeMap<u64, Vec<Transaction>>);
+pub struct TxBuffer {
+    transactions: BTreeMap<u64, Vec<Transaction>>,
+}
 
 impl TxBuffer {
     pub fn new() -> Self {
-        TxBuffer(BTreeMap::new())
+        TxBuffer {
+            transactions: BTreeMap::new(),
+        }
     }
 
     pub fn take_to_range(&mut self, end: u64) -> Vec<Transaction> {
-        let keys: Vec<u64> = self.0.range(..=end).map(|(&k, _)| k).collect();
-        keys.into_iter().filter_map(|k| self.0.remove(&k)).flatten().collect()
+        let keys: Vec<u64> = self.transactions.range(..=end).map(|(&k, _)| k).collect();
+
+        keys.into_iter().filter_map(|k| self.transactions.remove(&k)).flatten().collect()
     }
 
     pub fn take_all(&mut self) -> Vec<Transaction> {
-        let txs = self.0.values().flat_map(|txs| txs.iter().cloned()).collect();
-        self.0.clear();
+        let txs = self.transactions.values().flat_map(|txs| txs.iter().cloned()).collect();
+
+        self.transactions.clear();
         txs
     }
 
     pub fn contains_pending(&self) -> bool {
-        !self.0.is_empty()
+        !self.transactions.is_empty()
     }
 
     pub fn insert_at_height(&mut self, height: u64, txs: Vec<Transaction>) {
-        self.0.entry(height).or_default().extend(txs);
+        self.transactions.entry(height).or_default().extend(txs);
     }
 }
