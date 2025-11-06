37f1062b316effc098fa7bef3bf3e371f10ac023
Jonas <jonas@deltadevs.xyz>
2025-11-06 08:57:54 +0100

    feat(crosstarget): use macros instead of functions for cross-target tracing
    Otherwise the tracing macros module is used as the origin in tracing output.

--- crates/crosstarget/src/tracing.rs
+++ crates/crosstarget/src/tracing.rs
@@ -1,53 +1,103 @@
 //! Cross-platform logging abstraction that provides a unified interface for logging
 //! across `WebAssembly` (browser console) and native (tracing) targets.
 
-#[cfg(target_arch = "wasm32")]
-pub use console::{debug, error, info, trace, warn};
-#[cfg(not(target_arch = "wasm32"))]
-pub use tracing_macros::{debug, error, info, trace, warn};
-
-#[cfg(target_arch = "wasm32")]
-mod console {
-    pub fn info(msg: &str) {
-        web_sys::console::log_1(&msg.into());
-    }
-
-    pub fn warn(msg: &str) {
-        web_sys::console::warn_1(&msg.into());
-    }
-
-    pub fn error(msg: &str) {
-        web_sys::console::error_1(&msg.into());
-    }
-
-    pub fn debug(msg: &str) {
-        web_sys::console::debug_1(&msg.into());
-    }
-
-    pub fn trace(msg: &str) {
-        web_sys::console::trace_1(&msg.into());
-    }
+#[macro_export]
+macro_rules! info {
+    ($($arg:tt)*) => {{
+        #[cfg(target_arch = "wasm32")]
+        {
+            $crate::tracing::__wasm_log_info(&format!($($arg)*))
+        }
+        #[cfg(not(target_arch = "wasm32"))]
+        {
+            ::tracing::info!($($arg)*)
+        }
+    }};
 }
 
-#[cfg(not(target_arch = "wasm32"))]
-mod tracing_macros {
-    pub fn info(msg: &str) {
-        tracing::info!("{}", msg);
-    }
+#[macro_export]
+macro_rules! warn {
+    ($($arg:tt)*) => {{
+        #[cfg(target_arch = "wasm32")]
+        {
+            $crate::tracing::__wasm_log_warn(&format!($($arg)*))
+        }
+        #[cfg(not(target_arch = "wasm32"))]
+        {
+            ::tracing::warn!($($arg)*)
+        }
+    }};
+}
 
-    pub fn warn(msg: &str) {
-        tracing::warn!("{}", msg);
-    }
+#[macro_export]
+macro_rules! error {
+    ($($arg:tt)*) => {{
+        #[cfg(target_arch = "wasm32")]
+        {
+            $crate::tracing::__wasm_log_error(&format!($($arg)*))
+        }
+        #[cfg(not(target_arch = "wasm32"))]
+        {
+            ::tracing::error!($($arg)*)
+        }
+    }};
+}
 
-    pub fn error(msg: &str) {
-        tracing::error!("{}", msg);
-    }
+#[macro_export]
+macro_rules! debug {
+    ($($arg:tt)*) => {{
+        #[cfg(target_arch = "wasm32")]
+        {
+            $crate::tracing::__wasm_log_debug(&format!($($arg)*))
+        }
+        #[cfg(not(target_arch = "wasm32"))]
+        {
+            ::tracing::debug!($($arg)*)
+        }
+    }};
+}
 
-    pub fn debug(msg: &str) {
-        tracing::debug!("{}", msg);
-    }
+#[macro_export]
+macro_rules! trace {
+    ($($arg:tt)*) => {{
+        #[cfg(target_arch = "wasm32")]
+        {
+            $crate::tracing::__wasm_log_trace(&format!($($arg)*))
+        }
+        #[cfg(not(target_arch = "wasm32"))]
+        {
+            ::tracing::trace!($($arg)*)
+        }
+    }};
+}
 
-    pub fn trace(msg: &str) {
-        tracing::trace!("{}", msg);
-    }
+// Hidden helper functions for WASM logging macros
+#[cfg(target_arch = "wasm32")]
+#[doc(hidden)]
+pub fn __wasm_log_info(msg: &str) {
+    web_sys::console::log_1(&msg.into());
+}
+
+#[cfg(target_arch = "wasm32")]
+#[doc(hidden)]
+pub fn __wasm_log_warn(msg: &str) {
+    web_sys::console::warn_1(&msg.into());
+}
+
+#[cfg(target_arch = "wasm32")]
+#[doc(hidden)]
+pub fn __wasm_log_error(msg: &str) {
+    web_sys::console::error_1(&msg.into());
+}
+
+#[cfg(target_arch = "wasm32")]
+#[doc(hidden)]
+pub fn __wasm_log_debug(msg: &str) {
+    web_sys::console::debug_1(&msg.into());
+}
+
+#[cfg(target_arch = "wasm32")]
+#[doc(hidden)]
+pub fn __wasm_log_trace(msg: &str) {
+    web_sys::console::trace_1(&msg.into());
 }
