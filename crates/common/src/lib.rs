15f4d32dd74b5ed3b80de1e8d324c2ac0f6ad5f3
Jonas <jonas@deltadevs.xyz>
2025-11-17 11:31:22 +0100

    refactor(common): remove api and builder modules

--- crates/common/src/lib.rs
+++ crates/common/src/lib.rs
@@ -1,12 +1,7 @@
 pub mod account;
-pub mod api;
-pub mod builder;
 pub mod digest;
 pub mod operation;
 pub mod transaction;
 
 #[cfg(feature = "test_utils")]
 pub mod test_transaction_builder;
-
-#[cfg(test)]
-mod tests;
