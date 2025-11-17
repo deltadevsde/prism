8b9914c19c60a082ccffac8e05690fd0e2de2a84
Jonas <jonas@deltadevs.xyz>
2025-11-17 11:31:28 +0100

    refactor(client): use prism-api crate

--- crates/client/src/lib.rs
+++ crates/client/src/lib.rs
@@ -1,11 +1,10 @@
 mod http_client;
-mod prism_api;
+mod http_prism_api;
 
 pub use http_client::{PrismHttpClient, PrismHttpClientError};
+pub use prism_api::{PendingTransaction, PrismApi, PrismApiError, builder, types};
 pub use prism_common::{
     account::Account,
-    api::{PendingTransaction, PrismApi, PrismApiError, types::*},
-    builder,
     digest::Digest,
     operation::{ServiceChallenge, ServiceChallengeInput, SignatureBundle},
     transaction::{Transaction, UnsignedTransaction},
@@ -14,4 +13,4 @@ pub use prism_keys::{CryptoAlgorithm, Signature, SigningKey, VerifyingKey};
 pub use prism_serde::binary;
 
 #[cfg(feature = "mockall")]
-pub use prism_common::api::mock;
+pub use prism_api::mock;
